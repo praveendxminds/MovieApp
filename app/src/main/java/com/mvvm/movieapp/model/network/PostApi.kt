c1e35a8dc55fcca0b5d9f218f87b3af3c41cd8e3
praveendxminds <47963837+praveendxminds@users.noreply.github.com>
2019-09-06 18:30:28 +0530

    mvvm

---
+++ app/src/main/java/com/mvvm/movieapp/model/network/PostApi.kt
@@ -0,0 +1,14 @@
+package com.mvvm.movieapp.model.network
+
+import retrofit2.http.GET
+
+/**
+ * The interface which provides methods to get result of webservices
+ */
+interface PostApi {
+    /**
+     * Get the list of the pots from the API
+     */
+   // @GET("/posts")
+  //  fun getPosts(): Observable<List<Post>>
+}
